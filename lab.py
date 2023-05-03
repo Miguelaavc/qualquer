d3a8e1cf1d137ad4e07d925e1568d1c4d3345ad5
Miguelaavc <122412136+Miguelaavc@users.noreply.github.com>
2023-05-03 17:49:09 -0300

    Update lab.py

--- lab.py
+++ lab.py
@@ -1,14 +1,38 @@
 geral = []
 while True:
-    lista = []
-    nome = str(input("Nome: "))
-    lista.append(nome)
-    num = int(input("Número do telefone: "))
-    lista.append(num)
-    date = int(input("Data de nascimento (dd/mm/aaaa): "))
-    lista.append(date)
-    geral.append(lista)
-    cont = str(input("Você quer continuar ? (S/N)")).upper()
-    if cont == "N":
+    opcao= int(input('''\n========= MENU =========
+1. CADASTRAR          2. GERAL
+3. PESQUISAR          4. EXCLUIR
+5. SAIR
+escolha sua opção: '''))
+    if opcao == 1:
+        lista = []
+        nome = str(input("Nome: "))
+        lista.append(nome)
+        num = str(input("Número do telefone: "))
+        lista.append(num)
+        date = str(input("Data de nascimento (dd/mm/aaaa): "))
+        lista.append(date)
+        geral.append(lista)
+        print(f"\n{nome} foi CADASTRADO!")
+
+    elif opcao == 2:
+        var = open("dados.txt", "w")
+        #var.write()
+        for pessoas in range((len(geral))):
+            print("\n")
+            for dados in geral[pessoas]:
+                #print(f"{dados}")
+                #for i in range(2):
+                    var.write(dados)
+
+    elif opcao == 3:
+        #procurar o nome na linha do arquivo
+        pass
+
+    elif opcao == 4:
+        #remover pelo nome
+        pass
+
+    elif opcao == 5:
         break
-print(geral)
